30bc7a7bae4c1c211d43d6a309d9b84397a14d28
Rasmus <50022415+TehRamsus@users.noreply.github.com>
2020-12-28 10:52:31 -0800

    added files

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'Extended jail - Ramsus'
+version '1.0.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@RM_Framework/locale.lua',
+	'locales/fi.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@RM_Framework/locale.lua',
+	'locales/fi.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/main.lua'
+}
